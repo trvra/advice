5e358348b93c8e067f784ac299092b9ba6c14788
Trevor Adams <trevor.s.adams@gmail.com>
2016-06-19 01:08:00 -0700

    Fix bug in LINE_NUM calculation

--- advice.sh
+++ advice.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ADVICE_FILE=~/.advice
-LINE_NUM=$((${RANDOM} % `sed -n '/==SOURCES==/=' ${ADVICE_FILE}`))
+LINE_NUM=$((${RANDOM} % ((`sed -n '/==SOURCES==/=' ${ADVICE_FILE}`) - 2) + 1))
 head -${LINE_NUM} ${ADVICE_FILE} | tail -1
