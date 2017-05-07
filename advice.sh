95cd5fe6414895d49a618b9815e5711e5f605f72
Trevor Adams <trevor.s.adams@gmail.com>
2017-05-07 11:28:49 -0700

    Add dim coloring and simplify print

--- advice.sh
+++ advice.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 ADVICE_FILE=~/.advice
 LINE_NUM=$((${RANDOM} % ((`sed -n '/==SOURCES==/=' ${ADVICE_FILE}`) - 2) + 1))
-head -${LINE_NUM} ${ADVICE_FILE} | tail -1
+ADVICE=$(head -${LINE_NUM} ${ADVICE_FILE} | tail -1)
+echo -e "\033[2m\"${ADVICE}\""
